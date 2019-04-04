54c94489df024c82eaf43cab94b98024cca0bf74
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2019-04-04 00:09:33 -0300

    mais exemplos de namespace

--- core/namespaces/code/use/ArrayUtil.php
+++ core/namespaces/code/use/ArrayUtil.php
@@ -27,8 +27,18 @@ function min($array): float
 
 namespace ArrayUtil;
 
-use function \ArrayUtil\Arithmetic\array_sum as a_sum;
 use function \ArrayUtil\Stats\min;
+use function \ArrayUtil\Arithmetic\array_sum as a_sum;
 
-var_dump(a_sum([1, 2, 3, 4])); //=> float(10)
-var_dump(min([1, 2, 3, 4])); //=> float(1)
+$a = [1, 2, 3, 4];
+
+var_dump(\min($a));                 //=> int(1)
+var_dump(\ArrayUtil\Stats\min($a)); //=> float(1)
+var_dump(Stats\min($a));            //=> float(1)
+var_dump(min($a));                  //=> float(1)
+
+var_dump(\array_sum($a));                      //=> int(10)
+var_dump(\ArrayUtil\Arithmetic\array_sum($a)); //=> float(10)
+var_dump(Arithmetic\array_sum($a));            //=> float(10)
+var_dump(array_sum($a));                       //=> int(10)
+var_dump(a_sum($a));                           //=> float(10)
